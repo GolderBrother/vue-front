65de51367376a3d7c5fb39751aa78f489c7a21d5
GolderBrother <1204788939@qq.com>
2019-08-18 12:00:31 +0800

    This is front of vue+docker+nginx

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li v-for="(user) in users" :key="user.id">
+      {{user.id}}:{{user.name}}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'app',
+  data(){
+    return {
+      users:[]
+    }
+  },
+  methods: {
+    async getUsers(){
+      const res = await axios.get('http://localhost:3000/api/users');
+      if(!res) return;
+      this.users = res.data;
+    }
+  },
+  mounted(){
+    this.getUsers();
+  }
+}
+</script>
+
+<style>
+
+</style>
